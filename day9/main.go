eebf55ba3e2a5f3cb25d28a9c287d718c2a7e83f
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-11-01 23:06:04 +0530

    Add initial Go module and main program demonstrating if-else logic

---
+++ day9/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("If else  check")
+	loginCount := 69
+	var result string
+	if loginCount < 10 {
+		result = "Not comman"
+	}
+	fmt.Println(result)
+}
