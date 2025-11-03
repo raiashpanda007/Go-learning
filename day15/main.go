ef93416841120eab0702ca37654ddbfdd01f2a00
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-11-03 23:25:37 +0530

    Add initial Go module and main program to send JSON data to a server

---
+++ day15/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	
+)
+
+const URL = "http://localhost:3000/post"
+
+func main() {
+	PostJSONRequest()
+}
+
+func PostJSONRequest() {
+	requestBody := strings.NewReader(`
+		{
+			"message":"Ashwin is my name"
+		}
+	`);
+	response , err := http.Post(URL, "application/json",requestBody)
+	if err != nil {
+		panic(err)
+	}
+	data, err := io.ReadAll(response.Body);
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("data :: ", string(data));
+}
