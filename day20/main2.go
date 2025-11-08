3535457c6ae1d75932abaeef8e0286370619d3c7
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-11-08 19:28:56 +0530

    add day20/main2.go

---
+++ day20/main2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func task(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("task no :: ", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := range 1000000 {
+		wg.Add(1)
+		go task(i, &wg)
+	}
+
+	wg.Wait()
+}
